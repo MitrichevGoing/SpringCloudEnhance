706da3344948b3f76631fc157ff76e938571178b
mitrichev <953288178@qq.com>
2017-12-27 22:19:07 +0800

    ..

---
+++ com-base-service/src/main/java/com/mitrichev/Application.java
@@ -0,0 +1,22 @@
+package com.mitrichev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>Title: SpringCloudEnhance--com.mitrichev.Application</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2017:12 </p>
+ * <p>Create DateTime: 2017/12/27 下午10:15 </p>
+ *
+ * @author Potter
+ * @version v2
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
